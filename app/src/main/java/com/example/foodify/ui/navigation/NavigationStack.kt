a64e45f530507b8a60173df1c659578f451adc40
AhmedSamy02 <nname1858@gmail.com>
2025-08-19 14:38:41 +0300

    [➕ FR] Adding Navigation Stack

---
+++ app/src/main/java/com/example/foodify/ui/navigation/NavigationStack.kt
@@ -0,0 +1,19 @@
+package com.example.foodify.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.foodify.ui.screens.SplashScreen
+
+@Composable
+fun NavigationStack(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController, startDestination = Screen.Splash.route
+    ){
+        composable(Screen.Splash.route) {
+            SplashScreen()
+        }
+    }
+}
