d3159d3a714751dae2b92f52fde13d409ec63817
MirvtMohamed <120274557+MirvtMohamed@users.noreply.github.com>
2025-08-24 14:58:26 +0300

    coil downgrade

--- app/src/main/java/com/example/foodify/data/local/CollectionRecipeCrossRef.kt
+++ app/src/main/java/com/example/foodify/data/local/CollectionRecipeCrossRef.kt
@@ -1,8 +1,14 @@
 package com.example.foodify.data.local
 
 import androidx.room.Entity
+import androidx.room.Index
 
-@Entity(tableName = "collection_recipe_cross_ref", primaryKeys = ["collectionId", "recipeId"])
+@Entity(tableName = "collection_recipe_cross_ref", primaryKeys = ["collectionId", "recipeId"],
+    indices = [
+        Index(value = ["collectionId"]),
+        Index(value = ["recipeId"])
+    ]
+)
 data class CollectionRecipeCrossRef(
     val collectionId: Int,
     val recipeId: Int
